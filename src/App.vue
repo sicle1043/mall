eaa91cf45e64b313dfea46635533e1afc44f206e
sicle <1481971792@qq.com>
2020-10-17 14:13:12 +0800

    "首页导航"

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
       <router-view></router-view>
       <main-tab-bar></main-tab-bar>
-    </div>
   </div>
 </template>
 <script>
-import mainTabBar from "@/components/content/maintabbar/MainTabBar";
+import MainTabBar from "@/components/content/maintabbar/MainTabBar";
+
 export default {
   name: "App",
   components: {
-    mainTabBar,
+    MainTabBar,
   },
 };
 </script>
 <style>
+@import "./assets/css/base.css"
 </style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,14 +1,24 @@
 <template>
-  <h1>首页</h1>
+  <div id="home">
+    <nav-bar class="nav-home">
+      <template v-slot:center>购物街</template>
+    </nav-bar>
+    <h1>首页</h1>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
 export default {
-  name:'Home'
-
-}
+  name: "Home",
+  components: {
+    NavBar,
+  },
+};
 </script>
 
 <style>
-
+.nav-home{
+  background-color: var(--color-tint);
+}
 </style>
